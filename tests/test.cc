ff6379daf319b990dd63ff11a1593f6e4370c4ba
lcdtyph <lcdtyph@gmail.com>
2019-06-25 21:23:34 +0800

    update test

--- tests/test.cc
+++ tests/test.cc
@@ -9,9 +9,26 @@ int main() {
     auto t = std::make_shared<WorkerThread>();
 
     using namespace std::chrono_literals;
-    for (int i = 0; i < 1000; ++i) {
-        t->PushTask([](int x){ std::this_thread::sleep_for(20ms); std::cout << x << std::endl; }, i);
+    for (int i = 0; i < 10; ++i) {
+        t->PushTimer(
+            10 * i,
+            10,
+            [](int x){
+                std::cout << "delayed timer: " << x << std::endl;
+            },
+            i
+        );
     }
+    std::this_thread::sleep_for(1ms);
+    for (int i = 0; i < 100; ++i) {
+        t->PushTask(
+            [](int x){
+                std::cout << x << std::endl;
+            },
+            i
+        );
+    }
+    std::this_thread::sleep_for(1s);
     t->Join();
 
     return 0;
